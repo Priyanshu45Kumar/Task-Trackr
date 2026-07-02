6eca8be020687ef67fe4445e5db3071cea4e74b1
Priyanshu45Kumar <py297065@gmail.com>
2026-07-02 09:55:37 +0530

    Update frontend URL for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/tasks",
+  baseURL: "https://task-trackr-l3tt.onrender.com/api/tasks",
 });
 
 export default API;
